c82a47dcb3ed37b01ddb8c07e396c2e68c43ea07
Anisha1903 <anisham0219@gmail.com>
2024-07-30 13:30:46 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,22 +1,22 @@
+// page.js
+
 import Screen from "@/components/Screen";
-import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 export default function Home() {
-  // screen
   return (
-    <div className="bg-slate-700  w-full h-full overflow-hidden">
+    <div className="w-full h-full overflow-hidden">
       <Screen />
       <Toaster
         toastOptions={{
           success: {
             style: {
               background: "white",
-              border: "1px solid border: #644D9A",
+              border: "1px solid #644D9A",
             },
             iconTheme: {
               primary: "#180F2A",
-              // primary: "#3e2670",
               secondary: "#FFFFFF",
             },
           },
--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,3 +1,24 @@
+// import React from "react";
+// import TextMsg from "./TextMsg";
+// import BellCircle from "./BellCircle";
+// import SendButton from "./SendButton";
+
+// const Screen = () => {
+//   return (
+//     <div className="bg-[#2a2041] sm:my-6 my-0  mx-auto w-[375px] h-[780px] ">
+//         <TextMsg /> 
+//         <BellCircle />
+//       <SendButton/>
+//     </div>
+//   );
+// };
+
+// export default Screen;
+
+// components/Screen.jsx
+
+// components/Screen.jsx
+
 import React from "react";
 import TextMsg from "./TextMsg";
 import BellCircle from "./BellCircle";
@@ -5,12 +26,30 @@ import SendButton from "./SendButton";
 
 const Screen = () => {
   return (
-    <div className="bg-[#2a2041] sm:my-6 my-0  mx-auto w-[375px] h-[780px] ">
-        <TextMsg /> 
-        <BellCircle />
-      <SendButton/>
+    <div className="bg-gradient-to-b from-[#2a2041] to-[#000000] sm:my-6 my-0 mx-auto w-[375px] h-[750px]">
+      <TextMsg />
+      <BellCircle />
+      <SendButton />
     </div>
   );
 };
 
 export default Screen;
+
+// import React from "react";
+// import TextMsg from "./TextMsg";
+// import BellCircle from "./BellCircle";
+// import SendButton from "./SendButton";
+
+// const Screen = () => {
+//   return (
+//     <div className="bg-gradient-to-b from-[#2a2041] to-[#000000] sm:my-6 my-0 mx-auto w-[375px] h-[780px]">
+//       <TextMsg /> 
+//       <BellCircle />
+//       <SendButton/>
+//     </div>
+//   );
+// };
+
+// export default Screen;
+
