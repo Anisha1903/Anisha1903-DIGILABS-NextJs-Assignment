e885a6d8eaea7b29b0070addca77ed876c3619fc
Anisha1903 <anisham0219@gmail.com>
2024-07-30 12:05:57 +0530

    first commit

--- components/BellCircle.jsx
+++ components/BellCircle.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { PiBell } from "react-icons/pi";
-
-// const BellCircle = () => {
-//   return (
-//     <div className="w-[375px] h-[290px] mt-16 flex justify-center items-center mx-auto">
-//       {/* circles */}
-//         <div className="w-[375px]  h-[350px] flex justify-center items-center border-gradient">
-//           <div className="w-[300px] h-[300px]  flex justify-center items-center border-gradient">
-//             <div className="w-[222px] h-[222px]  flex justify-center items-center border-gradient">
-//               <div className="w-[160px] h-[160px]  bg-custom-gradient flex justify-center items-center rounded-full">
-//                 {/* <div className="w-[100px] h-[100px] bg-custom-gradient  flex justify-center items-center rounded-full"> */}
-//                 <PiBell className="w-[100px] h-[100px] opacity-95 text-white" />
-//                 {/* </div> */}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BellCircle;
-
 import React from "react";
 import { PiBell } from "react-icons/pi";
 
--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import StatusBar from "./StatusBar";
 import TextMsg from "./TextMsg";
 import BellCircle from "./BellCircle";
 import SendButton from "./SendButton";
 
 const Screen = () => {
   return (
-    <div className="bg-[#2a2041] sm:my-6 my-0  mx-auto w-[375px] h-[812px] ">
-      <StatusBar />
+    <div className="bg-[#2a2041] sm:my-6 my-0  mx-auto w-[375px] h-[780px] ">
         <TextMsg /> 
         <BellCircle />
       <SendButton/>
